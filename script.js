df0abf2c42b173e676326ce161bab971420dba53
Leandro Chaparro <leandrochaparro19@gmail.com>
2022-10-30 21:18:30 -0300

    portafolio completo falta responsividad

---
+++ script.js
@@ -0,0 +1,19 @@
+function abrirEncriptador(){
+    window.open('https://leandrochaparro19.github.io/Encriptador/', '_blank');
+}
+
+function abrirJuegoAhorcado(){
+    window.open('https://leandrochaparro19.github.io/Juego_Ahorcado/', '_blank');
+}
+
+function abrirInstagram(){
+    window.open('https://www.instagram.com/lea_chaparro/', '_blank');
+}
+
+function abrirGithub(){
+    window.open('https://github.com/leandrochaparro19', '_blank');
+}
+
+function abrirLinkedin(){
+    window.open('https://www.linkedin.com/in/leandro-chaparro-56310b246/', '_blank');
+}
